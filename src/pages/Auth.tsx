772879987005b9c714521cbf2fff219bb1937af4
Enzo Blois <enzoblois4@gmail.com>
2026-02-02 22:24:24 +0100

    feat(frontend): implement modern UI with Tailwund CSS v4

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -36,7 +36,7 @@ export default function Login() {
           </div>
         </div>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 tracking-tight">
-          Connexion à SportHub
+          Connexion à Sports Club
         </h2>
         <p className="mt-2 text-center text-sm text-gray-500">
           Entrez vos accès pour gérer vos clubs
